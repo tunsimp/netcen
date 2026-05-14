bcdd8ff99ef6f6fcc6a677e51fc5e2e85249cc62
tun <nguyenquoctuanwar@gmail.com>
2026-05-14 13:07:09 +0700

    Delete internal directory

--- internal/models/chat.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type ChatMessage struct {
-	Type      string `json:"type"`
-	UserID    string `json:"user_id"`
-	Username  string `json:"username"`
-	Message   string `json:"message"`
-	Timestamp int64  `json:"timestamp"`
-}
